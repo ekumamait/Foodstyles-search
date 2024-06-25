cba5619347880d9364e6c738cd98070e13dbb57f
ekumamait <ericebuluochol@gmail.com>
2024-06-25 23:26:10 +0300

    load credentials through orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: process.env.DB_TYPE,
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-    }),
-    SearchModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), SearchModule],
 })
 export class AppModule {}
